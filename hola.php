1e4c70e35ba57e724ce882afb5efcfe88a4bd3e3
marcosalvaradomartinez <mam02@iesemilidarder.com>
2023-03-21 18:26:59 +0100

    Cambios en hola.php 2.0

--- hola.php
+++ hola.php
@@ -2,27 +2,30 @@
 
 <body>
         <?php
-            DEFINE('var3',"!!");
-            $var1 = "Hola"; 
-            $var2 = "Mundo";
+            // DEFINE('var3',"!!");
+            // $var1 = "Hola"; 
+            // $var2 = "Mundo";
             
-            if($var1 == $var2){
-                echo "son iguales";
-            }elseif($var1 === $var2){
-                echo "son diferentes";
-            }else{
-                echo "parado no bailao";
-            }
+            // if($var1 == $var2){
+            //     echo "son iguales";
+            // }elseif($var1 === $var2){
+            //     echo "son diferentes";
+            // }else{
+            //     echo "parado no bailao";
+            // }
             
-            // $var1 = 10;
-            // $var2 = 0;
-            // $var3 = 0;
+            $var1 = 10;
+            $var2 = 0;
+            $var3 = 0;
 
-            // try (){
-            //     $var3 = $var1 / $var2;
-            //     echo $var3;
-            // } catch (){
-            //     echo "Se ha producido un error global";
+            try {
+                $var3 = $var1 / $var2;
+                echo $var3;
+            } catch (Error $e){
+                echo "Se ha producido un error global";
+            }
+            // }catch (Excepción $e){
+            //     echo "Se ha producido una excepción";
             // }
         ?>
 </body>
