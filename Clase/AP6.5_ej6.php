53a1cb9bc261ad38aa7057aca425c6f91b63becc
marcosalvaradomartinez <mam02@iesemilidarder.com>
2023-03-28 17:37:37 +0200

    Añadimos la carpeta Clase con cambios

--- Clase/AP6.5_ej6.php
+++ Clase/AP6.5_ej6.php
@@ -14,15 +14,15 @@
 // echo "Hay " . $pares . "pares y " . $impares . " impares."
 
 $cadena = array('x', 'l', 'a', 'b', 'l', 'a', 'c', 'l', 'd');
+$i = 0;
+$total=0;
 
-foreach ($cadena as $num) {
-    if(($num % 2) == 0){
-        $pares=$pares+1;
-    }else{
-        $impares=$impares+1;
-    }
-}
+for ($i = 0; $i <= sizeof($cadena); ($i++)) {
+    if(($cadena[$i] == 'l' ) && ($cadena[$i+1] == 'a')){
+        $total++;
+}   
+};
 
-echo "Hay " . $pares . "pares y " . $impares . " impares."
+echo "Hay " . $total;
 
 ?>
